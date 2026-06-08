e685dd9d02100671d08fb4efbe625368995a5fab
princy-art <princykittu@gmail.com>
2026-06-08 16:05:43 +0530

    Day-8 uploaded

---
+++ Day-8/29q.c
@@ -0,0 +1,18 @@
+// Write a program to Print half pyramid pattern//
+#include<stdio.h>
+int main()
+{
+    int i,j,row;
+    printf("Enter the number of row = ");
+    scanf("%d",&row);
+
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Day-8/31q.c
@@ -0,0 +1,23 @@
+/*Write a program to Print character triangle. 
+A 
+AB 
+ABC 
+ABCD 
+ABCDE*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+
+    for(i=1;i<=5;i++)
+    {  
+        char c='A';
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",c);
+            c =c+1;
+        }
+        printf("\n");
+    }
+    return 0;
+}
